686c5e221e023faa9e8cec1851b0a17c84446e44
StevenAston <steven.james.aston@gmail.com>
2014-08-14 14:14:00 -0400

    Merge conflicts

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-//var t1 = document.getElementById("#t1");
-//console.log("Topping 1 Checked");
-var run = true;
-
-function calcute() 
-{	
-	while (run == true) {
-		console.log("Running");
-		setTimeout(1000);
+	console.log("Script running");
+	function checkTotal() {
+		document.listForm.total.value = '';
+		var sum = 0.0;
+		for (i=0;i<document.listForm.choice.length;i++) {
+		  if (document.listForm.choice[i].checked) {
+		  	sum = sum + parseInt(document.listForm.choice[i].value);
+		  }
+		}
+		document.listForm.total.value = sum;
 	}
-}
-
-window.onload = calcute();
